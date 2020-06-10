97cbe54ac7482110b685b33d9e23d6a80cfd34e5
kitae104 <aqua0405@gmail.com>
2020-06-10 11:02:54 +0900

    자바 네트워크

---
+++ src/network/URLRead.java
@@ -0,0 +1,31 @@
+package network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLRead {
+
+	public static void main(String[] args) {
+
+		try {
+			URL url = new URL("https://www.naver.com/");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			String line = "";
+			while((line = br.readLine()) != null ) {
+				System.out.println(line);
+			}
+			br.close();
+			
+		} catch (MalformedURLException e) {			
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
