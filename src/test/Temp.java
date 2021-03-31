2a6c8cc1bcc1c408f2ee68934c361f973a4f859d
kitae104 <aqua0405@gmail.com>
2021-04-01 00:36:40 +0900

    테스트 파일

---
+++ src/test/Temp.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Temp {
+	
+	int a;
+	
+	public Temp(int b) {
+		a = b;
+	}
+	
+	public void AA() {
+		a = 3;
+		System.out.println(a);
+	}
+	
+	public void BB() {
+		int b = 4; 
+		int sum = a + b;
+		System.out.println(sum);
+	}
+	
+}
---
+++ src/test/TempTest.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TempTest {
+
+	public static void main(String[] args) {
+		Temp.b = 10;
+				
+		Temp t = new Temp();
+		Temp t1 = new Temp();
+
+	}
+
+}
