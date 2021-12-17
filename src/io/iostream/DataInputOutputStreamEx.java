8756e90c719cbc81f6864675b639fd7c450ccf26
kitae104 <aqua0405@gmail.com>
2021-12-18 02:39:56 +0900

    Update DataInputOutputStreamEx.java

--- src/io/iostream/DataInputOutputStreamEx.java
+++ src/io/iostream/DataInputOutputStreamEx.java
@@ -3,7 +3,6 @@ package io.iostream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 public class DataInputOutputStreamEx
