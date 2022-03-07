32609649266fdcc963adb5ffa695a06487d47a3c
Kitae's Github <aqua0405@gmail.com>
2022-03-07 09:46:16 +0900

    Create Frame1.java

---
+++ src/gui/basic/Frame1.java
@@ -0,0 +1,82 @@
+package gui.basic;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class Frame1
+{
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					Frame1 window = new Frame1();
+					window.frame.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Frame1()
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Show Message");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, textField.getText());
+			}
+		});
+		btnNewButton.setForeground(Color.RED);
+		btnNewButton.setBackground(Color.ORANGE);
+		btnNewButton.setFont(new Font("D2Coding", Font.PLAIN, 12));
+		btnNewButton.setBounds(154, 168, 136, 40);
+		frame.getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(154, 84, 185, 21);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("\uB370\uC774\uD130 :");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(71, 87, 57, 15);
+		frame.getContentPane().add(lblNewLabel);
+	}
+}
