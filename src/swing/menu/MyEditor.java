d85fe8f1596efc5bd935ecfb3e257774c4509cc1
Kitae's Github <aqua0405@gmail.com>
2023-04-19 02:08:16 +0900

    Update MyEditor.java

--- src/swing/menu/MyEditor.java
+++ src/swing/menu/MyEditor.java
@@ -2,7 +2,6 @@ package swing.menu;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -15,7 +14,6 @@ import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JColorChooser;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
