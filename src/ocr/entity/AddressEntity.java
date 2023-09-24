15d6463ba4e648c94eeef36beb05b07881d71e5e
Kitae's Github <aqua0405@gmail.com>
2023-09-25 01:23:28 +0900

    Create AddressEntity.java

---
+++ src/ocr/entity/AddressEntity.java
@@ -0,0 +1,30 @@
+package ocr.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressEntity {
+
+    private Long id;                        // 배송 정보 관리 번호
+
+    private int zipCode;                    // 우편번호;
+
+    private String sido;                    // 시도
+
+    private String gugun;                   // 구군
+
+    private String dong;                    // 동
+
+    private String li;                      // 리
+
+    private String bunji;                   // 번지
+
+    private String doro;                    // 도로명주소
+
+    private String detailAddress;           // 상세주소
+
+}
