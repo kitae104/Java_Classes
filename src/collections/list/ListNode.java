47e2d6066e719c4353288372f69df98605904f39
Kitae's Github <aqua0405@gmail.com>
2024-02-09 01:21:58 +0900

    List 처리

---
+++ src/collections/list/ListNode.java
@@ -0,0 +1,16 @@
+package collections.list;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ListNode {
+    String day;
+    ListNode link;
+
+    public ListNode(String day) {
+        this.day = day;
+        this.link = null;
+    }
+}
