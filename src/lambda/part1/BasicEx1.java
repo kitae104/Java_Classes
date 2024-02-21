777b5ba408073137b7325ddfd5f91498acd5c432
Kitae's Github <aqua0405@gmail.com>
2024-02-21 17:28:40 +0900

    Create BasicEx1.java

---
+++ src/lambda/part1/BasicEx1.java
@@ -0,0 +1,14 @@
+package lambda.part1;
+
+public class BasicEx1 {
+    public static void main(String[] args) {
+//        Object obj = (a, b) -> a > b ? a : b;
+//        Object obj = new Object(){
+//            int max(int a, int b){
+//                return a > b ? a : b;
+//            }
+//        };
+//
+//        int value = obj.max(10, 20);
+    }
+}
