cbd03b40b6d9b5deeb7393ba017033fb26b3ba6b
Kitae's Github <aqua0405@gmail.com>
2024-03-02 22:46:40 +0900

    Create TabEx.java

---
+++ src/swing/builder/components/TabEx.java
@@ -0,0 +1,95 @@
+package swing.builder.components;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TabEx
+{
+
+	private JFrame frmTab;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					TabEx window = new TabEx();
+					window.frmTab.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TabEx()
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frmTab = new JFrame();
+		frmTab.setTitle("Tab 연습");
+		frmTab.setBounds(100, 100, 676, 492);
+		frmTab.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTab.getContentPane().setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(38, 27, 570, 390);
+		frmTab.getContentPane().add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("New tab", null, panel, null);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "탭에 대한 연습중입니다.");
+			}
+		});
+		btnNewButton.setBounds(36, 37, 91, 23);
+		panel.add(btnNewButton);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("D:\\Github\\Java_WS\\Java_Classes\\images\\img1.jpg"));
+		lblNewLabel.setBounds(36, 23, 390, 293);
+		panel_1.add(lblNewLabel);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("D:\\Github\\Java_WS\\Java_Classes\\images\\java.jpg"));
+		lblNewLabel_1.setBounds(12, 10, 527, 325);
+		panel_2.add(lblNewLabel_1);
+	}
+}
