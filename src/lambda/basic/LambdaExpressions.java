e3dec882a3e455263627915470027d3a7590cee5
Kitae's Github <aqua0405@gmail.com>
2024-03-21 23:17:02 +0900

    Update LambdaExpressions.java

--- src/lambda/basic/LambdaExpressions.java
+++ src/lambda/basic/LambdaExpressions.java
@@ -7,17 +7,17 @@ public class LambdaExpressions
 {
 	public static void main(String[] args)
 	{
-		List<String> list = new ArrayList<>(List.of("aaa", "bbb", "ccc", "ddd"));
-		
+		List<String> list = new ArrayList<>(List.of("aaa", "bbb", "ccc", "ddd"));	// Java 9부터 지원
+
 		for (String s : list)
 		{
 			System.out.println(s);
 		}
 		
 		System.out.println("==================================");
-		list.forEach((s) -> System.out.println(s));
+		list.forEach((s) -> System.out.println(s));	// forEach 사용
 		
 		System.out.println("==================================");
-		list.forEach(s -> System.out.println(s));
+		list.forEach(s -> System.out.println(s)); 	// 람다식 사용
 	}
 }
