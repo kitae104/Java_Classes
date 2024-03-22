7443b26497532856d05ca375cab233a918e07298
Kitae's Github <aqua0405@gmail.com>
2024-03-23 00:06:32 +0900

    Update StreamMatch.java

--- src/lambda_stream/lastoper/StreamMatch.java
+++ src/lambda_stream/lastoper/StreamMatch.java
@@ -1,6 +1,9 @@
 package lambda_stream.lastoper;
 
+import lambda_stream.practice.Student;
+
 // allMatch(), anyMatch(), noneMatch() 메소드는 모두 최종 연산이다.
+// findFirst(), findAny() 메소드는 최종 연산이지만, findFirst()은 첫 번째 요소를 반환하고, findAny()는 임의의 요소를 반환한다.
 public class StreamMatch {
     public static void main(String[] args) {
 
