8ca1a8c4819c10d81588ccb6c8c76fd7821ef8b4
Kitae's Github <aqua0405@gmail.com>
2025-04-30 00:56:30 +0900

    Update ListFrame.java

--- src/part2/components/ListFrame.java
+++ src/part2/components/ListFrame.java
@@ -6,27 +6,24 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class ListFrame
-{
+public class ListFrame {
 
 	private JFrame frmList;
-	private JList list;
+	private JList lstName;
+	private JTextField tfName;
+	private DefaultListModel<String> listModel;
+	private DefaultComboBoxModel<String> comboModel;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args)
-	{
-		EventQueue.invokeLater(new Runnable()
-		{
-			public void run()
-			{
-				try
-				{
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
 					ListFrame window = new ListFrame();
 					window.frmList.setVisible(true);
-				} catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -36,48 +33,67 @@ public class ListFrame
 	/**
 	 * Create the application.
 	 */
-	public ListFrame()
-	{
+	public ListFrame() {
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize()
-	{
+	private void initialize() {
 		frmList = new JFrame();
 		frmList.setTitle("LIST 연습하기");
-		frmList.setBounds(100, 100, 536, 361);
+		frmList.setBounds(100, 100, 411, 329);
 		frmList.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmList.getContentPane().setLayout(null);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(12, 10, 91, 23);
-		frmList.getContentPane().add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(115, 14, 50, 15);
-		frmList.getContentPane().add(lblNewLabel);
-		
-		JButton btnNewButton_1 = new JButton("Load Value");
-		btnNewButton_1.addActionListener(new ActionListener() {
+
+		JButton btnAdd = new JButton("추가");
+		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// 리스트에는 모델을 사용해야 함 
-				DefaultListModel<String> model = new DefaultListModel<>();
-				model.addElement("AAA");
-				model.addElement("BBB");
-				model.addElement("CCC");
-				model.addElement("DDD");
-				model.addElement("EEE");
-				list.setModel(model);
+				String name = tfName.getText();
+				if (name.equals("")) {
+					JOptionPane.showMessageDialog(frmList, "이름을 입력하세요");
+					return;
+				}
+				listModel.addElement(name);
+				tfName.setText("");
 			}
 		});
-		btnNewButton_1.setBounds(32, 132, 126, 23);
-		frmList.getContentPane().add(btnNewButton_1);
-		
-		list = new JList();
-		list.setBounds(198, 49, 294, 230);
-		frmList.getContentPane().add(list);
+		btnAdd.setBounds(253, 21, 132, 23);
+		frmList.getContentPane().add(btnAdd);
+
+		JButton btnToCombo = new JButton(">>");
+		btnToCombo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int index = lstName.getSelectedIndex();
+				if (index == -1) {
+					JOptionPane.showMessageDialog(frmList, "이름을 선택하세요");
+					return;
+				}
+				String name = listModel.get(index);
+				comboModel.addElement(name);
+				listModel.remove(index);
+			}
+		});
+		btnToCombo.setBounds(176, 106, 65, 23);
+		frmList.getContentPane().add(btnToCombo);
+
+		listModel = new DefaultListModel<String>();
+
+		lstName = new JList();
+		lstName.setModel(listModel);
+		lstName.setBounds(12, 53, 147, 230);
+		frmList.getContentPane().add(lstName);
+
+		tfName = new JTextField();
+		tfName.setBounds(12, 22, 229, 21);
+		frmList.getContentPane().add(tfName);
+		tfName.setColumns(10);
+
+		comboModel = new DefaultComboBoxModel<String>();
+		JComboBox cbName = new JComboBox();
+		cbName.setModel(comboModel);
+		cbName.setBounds(269, 54, 119, 23);
+		frmList.getContentPane().add(cbName);
 	}
 }
