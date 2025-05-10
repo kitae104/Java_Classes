e510f618231f95196e2aec5539acfbd9f20e387a
Kitae's Github <aqua0405@gmail.com>
2025-05-10 22:45:33 +0900

    IO 스트림 처리

--- src/io/readerwriter/InputStreamReaderEx.java
+++ src/io/readerwriter/InputStreamReaderEx.java
@@ -5,42 +5,30 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class InputStreamReaderEx
-{
+public class InputStreamReaderEx {
 
-	public static void main(String[] args)
-	{
-		InputStream is = System.in;
-		Reader reader = new InputStreamReader(is);
-		
-		int readCharNo;
-		char[] cbuf = new char[100];
-		
-		try
-		{
-			while((readCharNo = reader.read(cbuf)) != -1) // ctrl + z 
-			{
-				String data = new String(cbuf, 0, readCharNo);
-				System.out.println(data);
-			}
-		} 
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			try
-			{
-				reader.close();
-				is.close();
-			} 
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-			
-		}
-	}
+  public static void main(String[] args) {
+    InputStream is = System.in;
+    Reader reader = new InputStreamReader(is);
 
+    int readCharNo;
+    char[] cbuf = new char[100];
+
+    try {
+      while ((readCharNo = reader.read(cbuf)) != -1) // ctrl + z
+      {
+        String data = new String(cbuf, 0, readCharNo);
+        System.out.println(data);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        reader.close();
+        is.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
--- src/io/readerwriter/ReaderEx1.java
+++ src/io/readerwriter/ReaderEx1.java
@@ -5,43 +5,31 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 
-public class ReaderEx1
-{
+public class ReaderEx1 {
 
-	public static void main(String[] args)
-	{
-		Reader reader = null;
-		try
-		{
-			reader = new FileReader("C:/Temp/test.txt");
-			int readData;
-			
-			while((readData = reader.read()) != -1)
-			{
-				System.out.println((char)readData);
-			}
-			
-		} 
-		catch (FileNotFoundException e)
-		{
-			System.out.println("파일을 찾는 중 오류 발생.");
-			e.printStackTrace();
-		} 
-		catch (IOException e)
-		{
-			System.out.println("입출력시 오류 발생.");
-			e.printStackTrace();
-		}
-		finally {
-			try
-			{
-				reader.close();
-			} 
-			catch (IOException e)
-			{
-				System.out.println("파일을 닫는 과정에서 오류가 발생했습니다.");
-				e.printStackTrace();
-			}
-		}
-	}
+  public static void main(String[] args) {
+    Reader reader = null;
+    try {
+      reader = new FileReader("C:/Temp/test.txt");
+      int readData;
+
+      while ((readData = reader.read()) != -1) {
+        System.out.println((char) readData);
+      }
+
+    } catch (FileNotFoundException e) {
+      System.out.println("파일을 찾는 중 오류 발생.");
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("입출력시 오류 발생.");
+      e.printStackTrace();
+    } finally {
+      try {
+        reader.close();
+      } catch (IOException e) {
+        System.out.println("파일을 닫는 과정에서 오류가 발생했습니다.");
+        e.printStackTrace();
+      }
+    }
+  }
 }
