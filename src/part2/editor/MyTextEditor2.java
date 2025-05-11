a9e5684507c5915d3e6ce004f622554a6c2826ee
Kitae's Github <aqua0405@gmail.com>
2025-05-11 22:50:20 +0900

    Update MyTextEditor2.java

--- src/part2/editor/MyTextEditor2.java
+++ src/part2/editor/MyTextEditor2.java
@@ -6,10 +6,18 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -17,6 +25,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JSeparator;
 import javax.swing.JToolBar;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
@@ -77,9 +86,19 @@ public class MyTextEditor2 {
 		mnNewMenu.add(mItemNew);
 		
 		JMenuItem mItemOpen = new JMenuItem("Open");
+		mItemOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openFunc();
+			}
+		});
 		mnNewMenu.add(mItemOpen);
 		
 		JMenuItem mItemSave = new JMenuItem("Save");
+		mItemSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				saveFunc();
+			}
+		});
 		mnNewMenu.add(mItemSave);
 		
 		JSeparator separator = new JSeparator();
@@ -159,10 +178,20 @@ public class MyTextEditor2 {
 		toolBar.add(btnNew);
 		
 		JButton btnOpen = new JButton("");
+		btnOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openFunc();
+			}
+		});
 		btnOpen.setIcon(new ImageIcon("D:\\Github\\Java_WS\\Java_Classes\\images\\open.png"));
 		toolBar.add(btnOpen);
 		
 		JButton btnSave = new JButton("");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				saveFunc();
+			}
+		});
 		btnSave.setIcon(new ImageIcon("D:\\Github\\Java_WS\\Java_Classes\\images\\save.png"));
 		toolBar.add(btnSave);
 		
@@ -172,10 +201,78 @@ public class MyTextEditor2 {
 				exitFunc();
 			}
 		});
-		btnExit.setIcon(new ImageIcon("D:\\Github\\Java_WS\\Java_Classes\\images\\exit.png"));
+		btnExit.setIcon(new ImageIcon(MyTextEditor2.class.getResource("/part2/images/exit.png")));
 		toolBar.add(btnExit);
 	}
-
+	
+	private void saveFunc() {
+		JFileChooser fc = new JFileChooser(); // 파일 선택 대화상자 생성
+		
+		fc.showSaveDialog(frame);	// 저장 대화상자 호출
+		
+		File out = fc.getSelectedFile();	// 선택된 파일
+		BufferedWriter bw = null;
+		
+		try {
+			bw = new BufferedWriter(new FileWriter(out));
+			String str = taEditor.getText();	// JTextArea의 내용 가져오기
+			str = str.replace("\n", System.getProperty("line.separator"));	// 줄바꿈 문자 변환
+			bw.write(str);	// 파일에 내용 쓰기
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bw != null) 
+					bw.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private void openFunc() {
+		JFileChooser fc = new JFileChooser();
+		
+		File currentDir = new File(".");
+		fc.setCurrentDirectory(currentDir);	// 현재 디렉토리 설정
+		
+		fc.addChoosableFileFilter(new FileNameExtensionFilter("Java", "java"));
+		fc.addChoosableFileFilter(new FileNameExtensionFilter("TEXT", "txt"));
+		fc.addChoosableFileFilter(new FileNameExtensionFilter("Images", "jpg", "png", "gif", "bmp"));
+		fc.setAcceptAllFileFilterUsed(true);
+		
+		fc.showOpenDialog(frame);
+		
+		File in = fc.getSelectedFile();
+		BufferedReader br = null;
+		String line = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(in));
+			while ((line = br.readLine()) != null) {
+				taEditor.append(line + "\n");
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("파일을 찾을 수 없습니다.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("파일을 읽는 중 오류가 발생했습니다.");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("알 수 없는 오류가 발생했습니다.");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null) 
+					br.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+	}
+	
 	private void exitFunc() {
 		int result = JOptionPane.showConfirmDialog(frame, "정말 종료하시겠습니까?", "종료 확인", JOptionPane.YES_NO_OPTION);
 		if (result == JOptionPane.YES_OPTION) {
